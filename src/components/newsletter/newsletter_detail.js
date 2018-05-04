fd373ebf41f1db57b826e74404479ca053467888
neilmyers <mmyersneil@gmail.com>
2018-05-04 15:41:51 -0600

    setup action and reducer for fetching the item detail

--- src/components/newsletter/newsletter_detail.js
+++ src/components/newsletter/newsletter_detail.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 class NewsletterDetail extends Component {
+
+  componentWillMount() {
+    // this.props.fetchedItemById()
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +17,10 @@ class NewsletterDetail extends Component {
   }
 }
 
-export default NewsletterDetail;
+function mapStateToProps() {
+  return {
+    fetchedItem: state.fetchedItem
+  }
+}
+
+export default connect(mapStateToProps, actions)(NewsletterDetail);
