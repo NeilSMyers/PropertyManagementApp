63e56c1f4d381575c72b22a9745f51c815c4f5c2
neilmyers <mmyersneil@gmail.com>
2018-05-04 11:33:24 -0600

    added newsletter route

---
+++ src/components/newsletter/newsletter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Newsletter extends Component {
+  render() {
+    return (
+      <div>
+        Newsletter Component
+      </div>
+    )
+  }
+}
+
+export default Newsletter;
