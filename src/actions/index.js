8c4ac31bd5b4b73503cd40bc830594b007fd0b63
neilmyers <mmyersneil@gmail.com>
2018-05-04 13:40:28 -0600

    setup action and get request to newsletter archive

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { FETCH_NEWSLETTER_ARCHIVE } from './types';
+
 import axios from 'axios';
 import history from '../history';
 
@@ -14,6 +16,20 @@ export function signinUser({email, password}) {
       })
       .catch(error => {
         console.log(error);
+      })
+  }
+}
+
+export function fetchNewsletterArchive() {
+  return function(dispatch) {
+    axios.get(`${ROOT_URL}/newsletterArchive`, {
+      headers: { authorization: localStorage.getItem('token') }
+    })
+      .then(response => {
+        dispatch({
+          type: FETCH_NEWSLETTER_ARCHIVE,
+          payload: response.data
+        })
       })
   }
 }
