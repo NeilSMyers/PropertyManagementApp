ed87b813c316de4a07ef466f36d6336b7d5dca88
neilmyers <mmyersneil@gmail.com>
2018-05-10 15:07:10 -0600

    added addTitle component and applied to sign in and sign up components

---
+++ src/components/HOC/addTitle.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+const addTitle = (Child, title) => {
+  return class extends Component {
+    render() {
+      return (
+        <div>
+          <div className="add-title"></div>
+          <div className="add-title__text">
+            {title}
+          </div>
+            <Child/>
+        </div>
+      )
+    }
+  }
+}
+
+export default addTitle;
